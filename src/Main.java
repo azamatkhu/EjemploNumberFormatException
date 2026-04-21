0c399f2e24f15044695a779b72aeb2233347743d
azamatkhu <azamat.khu@educa.jcyl.es>
2026-04-21 13:19:20 +0200

    Bulk upload

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Dime un numero: ");
+        String n = sc.nextLine();
+        // Utilizamos try para intentar ejecutar una secuencia de codigo
+        // Esto sirve para capturar los errores posibles durante ejecucion de codigo
+        try {
+            int numero = Integer.parseInt(n);
+            System.out.println("Numero introducido: " + numero);
+        // catch - captura los errores y los sale por la pantalla
+        } catch (NumberFormatException e) {
+            System.out.println("Numero introducido no valido");
         }
     }
 }
